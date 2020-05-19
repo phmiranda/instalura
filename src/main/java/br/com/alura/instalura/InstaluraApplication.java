82bb00c0f11a5f7021a3e5a5b6e27b483d0e8a6f
phmiranda <pehhagah.1607@gmail.com>
2020-05-19 20:06:11 -0300

    [add] - instalação do spring boot no projeto para a construção de uma API.

---
+++ src/main/java/br/com/alura/instalura/InstaluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.instalura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/instalura/InstaluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.instalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
